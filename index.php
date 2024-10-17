e0ac98e217be530a853e8b181c3b02dfa6a52a4b
sinisti <spideralan2008@gmail.com>
2024-10-17 11:17:27 +0200

    idk

--- index.php
+++ index.php
@@ -3,39 +3,27 @@ header('Content-Type: text/html; charset=UTF-8');
 require('app/config/config.php');
 require('app/config/db.php');
 require('app/functions/validate.function.php');
-require('app\functions\helper.function.php');	
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{	
+require('app/functions/helper.function.php');	
+if($_SERVER['REQUEST_METHOD'] == 'POST'){	
 	fieldRequired('Imię', $_POST['name']);
 	fieldRequired('Nazwisko', $_POST['surname']);
 	fieldRequired('E-mail', $_POST['email']);
 	fieldRequired('Hasło', $_POST['password']);
-	if(!$isError)
-	{
+	if(!$isError){
 		isEmail('E-mail', $_POST['email']);
 	}
-
-	if (!$isError)
-	{	
+	if (!$isError){	
 		$password = md5(PASS_SALT . $_POST['password']);
 		$query = "INSERT INTO users SET user_name = '{$_POST['name']}', user_surname = '{$_POST['surname']}', user_email = '{$_POST['email']}', user_password = '$password'";
-		if ($db->query($query))
-		{
-			echo '<div class="alert alert-success" role="alert">
-  				  Data inserted succesfully!
-				  </div>';
+		if ($db->query($query)){
+			showMessage('success', 'Data inserted succesfully!');
 		}
-		else
-		{
-			echo '<div class="alert alert-danger" role="alert">
-  				  Data has not been inserted!
-				  </div>';
+		else{
+			showMessage('warning', 'Data has not been inserted!');
 		}
 	}
 }
-
 ?>
-
 <!DOCTYPE html>
 <html data-bs-theme="dark">
     <head>
@@ -45,16 +33,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 
 		<script type="text/javascript" src="assets/js/bootstrap/bootstrap.min.js"></script>
 	</head>
-	
 	<body>
 		<main>
 			<section class="content">
 				<?php include ('templates/form.html.php'); ?>
 			</section>
 			<section class="content">
-				
-
-				<?php include ('templates/users.html.php'); ?>
+				<?php
+				if(isset($_REQUEST['action'])){	
+					$action = $_REQUEST['action'];
+					switch($action){
+						case 'delete':
+							if(isset($_REQUEST['id']) && !empty($_REQUEST['id'])){
+								$id = (int) $_REQUEST['id'];
+							}
+							$query = "DELETE FROM users WHERE id=$id";
+							if ($db->query($query)){
+								showMessage('success', 'Data deleted succesfully!');
+							}
+							else{
+								showMessage('warning', 'Data has not been deleted!');
+							}
+							break;
+					}
+				}
+					include ('templates/users.html.php');	
+				?>
 			</section>
 		</main>
 	</body>
